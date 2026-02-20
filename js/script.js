04f78a6e3beb616eeb0b0701b2c25613a2ac45dd
sarah_syed <sarahsyed2009@gmail.com>
2026-02-19 23:07:16 -0600

    Creating a new function to fecth user data

--- js/script.js
+++ js/script.js
@@ -1,10 +1,28 @@
 //This is my target where my profile will appear
 const overview = document.querySelector(".overview");
-const username = document.querySelector("sarahsyed");
+const username = "sarahsyed";
 
 const profile = async function(){
- const data = await fetch(`https://api.github.com/target:users/${username}`);
- const res = data.json;
- console.log(res);
+ const res = await fetch(`https://api.github.com/users/${username}`);
+ const data = await res.json();
+ console.log(data);
+ userData(data);
+ //console.log(data.public_repos);
 }
 profile();
+
+const userData = function(data){
+    const userInfo = document.createElement("div");
+        const name = data.name;
+        const bio = data.bio;
+        const location = data.location;
+        const repos = data.public_repos;
+        userInfo.innerText = name;
+    
+}
+
+    
+
+    
+    
+    
