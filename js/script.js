e7f6e95305984a5adf18703bee2a3b2c32ffc486
sarah_syed <sarahsyed2009@gmail.com>
2026-03-03 00:05:28 -0600

    Added a function to display user data on the screen

--- js/script.js
+++ js/script.js
@@ -7,22 +7,30 @@ const profile = async function(){
  const data = await res.json();
  console.log(data);
  userData(data);
- //console.log(data.public_repos);
+   
 }
 profile();
 
 const userData = function(data){
-    const userInfo = document.createElement("div");
-        const name = data.name;
-        const bio = data.bio;
-        const location = data.location;
-        const repos = data.public_repos;
-        userInfo.innerText = name;
+    const div = document.createElement("div");
+      
+        div.classList.add("user-info");
+        div.innerHTML = (`
+        <figure>
+        <img alt="user avatar" src= ${data.avatar_url}/>
+        </figure>
+    <div>
+      <p><strong>Name:</strong>${data.name}</p>
+      <p><strong>Bio:</strong> ${data.bio}</p>
+      <p><strong>Location:</strong> ${data.location}</p>
+      <p><strong>Number of public repos:</strong> ${data.public_repos}</p>
+    
+    </div> 
+        `)
+     overview.append(div);   
     
 }
 
     
 
-    
-    
     
